c0867d76ae76091ff14e118fb11cfef5d0e8a13d
gardenOctopus <zebraspider@protonmail.com>
2023-08-18 16:04:24 -0400

    Added image to homepage

--- src/home.js
+++ src/home.js
@@ -1,1 +1,11 @@
-//Asset
+import homeImage from './images/MangoNirvanaHomeSmall.jpg';
+
+function homePage () {
+    const image = new Image();
+    image.src = homeImage;
+    image.alt = 'Mango Sticky Rice';
+
+    return image;
+}
+
+export default homePage;
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,11 +1,13 @@
 import header from './nav-menu';
 import './style.css';
+import homePage from './home';
 
 function pageLoad() {
     const body = document.querySelector('body');
     body.classList.add('body');
     const content = document.querySelector('#content');
     content.appendChild(header());
+    content.appendChild(homePage());
 }
 
 export default pageLoad;
