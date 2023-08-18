6de3cd3931ca562472b48c7b4742d087868a8936
gardenOctopus <zebraspider@protonmail.com>
2023-08-18 06:33:36 -0400

    Added styles to navbar

--- src/nav-menu.js
+++ src/nav-menu.js
@@ -1,18 +1,26 @@
+import './style.css';
+
 function header() {
     //Creates Header Object
     const header = document.createElement('header');
+    header.classList.add('header');
 
     //Creates Logo & Adds to Header 
     const logo = document.createElement('h1');
     logo.textContent = 'Mango Nirvana';
     header.appendChild(logo);
 
+    //Creates Links
+    const linkContainer = document.createElement('div');
+    header.appendChild(linkContainer);
+
     const links = ['About', 'Menu', 'Contact'];
     links.forEach(createHeaderLink);
     function createHeaderLink(link) {
         let x = document.createElement('a');
+        x.classList.add('headerLink');
         x.textContent = link;
-        header.appendChild(x);
+        linkContainer.appendChild(x);
     }
 
     return header;
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,6 +1,9 @@
 import header from './nav-menu';
+import './style.css';
 
 function pageLoad() {
+    const body = document.querySelector('body');
+    body.classList.add('body');
     const content = document.querySelector('#content');
     content.appendChild(header());
 }
